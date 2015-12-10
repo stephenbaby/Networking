02c85764e5fbf20649e5a9882693eaf0e028397a
stephenbaby <wongfx@foxmail.com>
2015-12-10 10:31:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wangfx/networking/NetworkingActivity.java
@@ -0,0 +1,122 @@
+package com.example.wangfx.networking;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+/**
+ * Created by wangfx on 2015/12/2.
+ */
+public class NetworkingActivity  extends Activity {
+    ImageView img;
+    private InputStream OpenHttpConnection (String urlString) throws IOException {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try {
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex) {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+
+    private Bitmap DownloadImage(String URL) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IOException el) {
+            Log.d("NetworkingActivity", el.getLocalizedMessage());
+        }
+        return bitmap;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        protected Bitmap doInBackground(String... urls) {
+            return DownloadImage(urls[0]);
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            ImageView img = (ImageView) findViewById(R.id.img);
+            img.setImageBitmap(result);
+        }
+    }
+
+    private String DownloadText(String URL){
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String str = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+        try {
+            while ((charRead = isr.read(inputBuffer)) > 0) {
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+        return str;
+    }
+
+    private class DownloadTextTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return DownloadText(urls[0]);
+        }
+    }
+
+//    @Override
+//    protected void onPostExecute(String result) {
+//        Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG);
+//        show();
+//    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        new DownloadImageTask().execute("http://www.mayoff.com/5-01cablecarDCP01934.jpg");
+    }
+}
